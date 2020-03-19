583a9c79719ccf44475ca4908eea9812c531dcd5
= <rickyalext@gmail.com>
2020-03-19 15:55:08 +0700

    first commit

---
+++ precache-manifest.442f13c62a5a9a791e83af61b38ef579.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b853677968385cbeb041c30e08653646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9328df665ee6766877b0",
+    "url": "/static/css/main.2ce4f82f.chunk.css"
+  },
+  {
+    "revision": "5c4fbb001735667aadfe",
+    "url": "/static/js/2.2b677d32.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.2b677d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9328df665ee6766877b0",
+    "url": "/static/js/main.cfacad3c.chunk.js"
+  },
+  {
+    "revision": "6d6351b50432bc230e96",
+    "url": "/static/js/runtime-main.19241b1e.js"
+  }
+]);
